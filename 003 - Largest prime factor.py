2caaad71692af483f739c87d87149f85878b8260
Albo <alberto.alborghetti@gmail.com>
2022-08-02 15:01:31 +0000

    commit

--- 003 - Largest prime factor.py
+++ 003 - Largest prime factor.py
@@ -3,3 +3,12 @@ The prime factors of 13195 are 5, 7, 13 and 29.
 
 What is the largest prime factor of the number 600851475143 ?
 """
+
+num , div= 600851475143, 1
+
+while num != 1:
+  div += 1
+  while (num % div) == 0:
+    num /= div
+
+print(div)
